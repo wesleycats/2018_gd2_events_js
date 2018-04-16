e0d8872e511948e8129303587c1408225e006983
Berend Weij <info@berendweij.nl>
2018-04-16 11:36:07 +0200

    adds comments

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Enemy from './enemy';
 
-console.log('main file working');
-
+// we maken een 'instantie' aan van de Class Enemy
 const endBoss = new Enemy();
 
-endBoss.addListener('death',
-                    () => console.log('ik zie dat de enemy dood is gegaan'));
+// vervolgens gaan we luisteren of de endBoss schreeuwt dat hij dood gaat
+endBoss.addListener('death',() => console.log('endBoss is dood. Ik zou nu iets nieuws kunnen spawnen'));
 
+// je mag zo vaak luisteren als je wil
+endBoss.addListener('death',() => console.log('endBoss is dood. Laten we de HUD updaten'));
+
+// nu gaan we net doen of we de endBoss 3x slaan
 endBoss.getDamage(20);
 endBoss.getDamage(10);
 endBoss.getDamage(70);
-console.log(endBoss);
